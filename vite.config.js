f0440b23af772a76a7e924e556b97b47f45da32b
Ben10 <haroldfulgar021@gmail.com>
2026-08-18 11:04:15 +0800

    Configure Vite to build to /docs folder for GitHub Pages deployment from main branch

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/LikeAndDislike/',
+  build: {
+    outDir: './docs',
+    emptyOutDir: true,
+    minify: false,
+  },
 })
