4ea447b3dda19e5e5f0ad54bcad4228eedef74bf
Ben10 <haroldfulgar021@gmail.com>
2026-08-18 09:41:34 +0800

    Updates

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ContentRating from './Components/ContentRating';
+
+function App() {
+  return (
+    <div className="app-shell">
+      <ContentRating />
+    </div>
+  );
+}
+
+export default App;
